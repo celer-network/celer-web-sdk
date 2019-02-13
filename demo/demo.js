9c17b4a2fe2754df9e328ccaa492b2abe1125716
Michael Zhou <zhoumotongxue008@gmail.com>
2019-02-12 20:48:12 -0800

    Use the public openEthChannel API in demo/demo.js

--- demo/demo.js
+++ demo/demo.js
@@ -2,7 +2,7 @@ const celer = require('../browser/browser'); // '../dist/index' for NodeJS
 
 const client = new celer.Client('http://localhost:29979');
 client
-  .openChannel('0', '0', '100', '100')
+  .openEthChannel('100', '100')
   .then(cid => {
     console.log('channel', cid, 'opened');
   })
