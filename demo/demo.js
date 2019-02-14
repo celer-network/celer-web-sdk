1e43834a843954b7d29727801e6b956751fce56e
Michael Zhou <zhoumotongxue008@gmail.com>
2019-02-14 11:42:08 -0700

    Add sendEth() to demo/demo.js
    Also change to use async / await to make the code cleaner.

--- demo/demo.js
+++ demo/demo.js
@@ -1,11 +1,18 @@
 const celer = require('../browser/browser'); // '../dist/index' for NodeJS
 
 const client = new celer.Client('http://localhost:29979');
-client
-  .openEthChannel('100', '100')
-  .then(cid => {
-    console.log('channel', cid, 'opened');
-  })
-  .catch(e => {
-    console.log(e);
-  });
+
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+(async function() {
+  const channelID = await client.openEthChannel('100', '100');
+  console.log('channel', channelID, 'has been opened');
+  const balanceBefore = await client.getEthBalance();
+  console.log('balance before', balanceBefore);
+  await client.sendEth('1', 'f805979adde8d63d08490c7c965ee5c1df0aaae2');
+  await timeout(1000);
+  const balanceAfter = await client.getEthBalance();
+  console.log('balance after', balanceAfter);
+})().catch(console.log);
